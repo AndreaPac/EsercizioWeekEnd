ef9b55881152ca4df805f953cc11713510e6bd16
Andrea Paciocca <81762546+AndreaPac@users.noreply.github.com>
2021-07-17 13:26:52 +0200

    server e client aggiunti

---
+++ server3/src/main/java/it/rand/demo/EsercizioWeekEndApplication.java
@@ -0,0 +1,13 @@
+package it.rand.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioWeekEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioWeekEndApplication.class, args);
+	}
+
+}
